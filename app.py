7676ed544800297ae16d9c0331ebc0c3700e5741
Harsh upadhyay <134951584+harsh22b1543003@users.noreply.github.com>
2025-03-17 22:37:44 +0530

    app.py

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 from flask import Flask
+import os
+import time
+import subprocess
+
 app = Flask(_name_)
 
-@app.route('/')
-def home():
-    return "Hello, Flask is running in Codespaces!"
+@app.route('/htop')
+def htop_info():
+    # Get system details
+    name = "Your Name"
+    username = os.getenv("USER", "codespace")
+    server_time = time.strftime("%Y-%m-%d %H:%M:%S IST")
 
-if _name_ == "_main_":
+    # Get htop output using top command
+    top_output = subprocess.getoutput("top -b -n 1")
+
+    # Format output as HTML
+    return f"""
+    <h1>System Information</h1>
+    <p><b>Name:</b> {name}</p>
+    <p><b>Username:</b> {username}</p>
+    <p><b>Server Time (IST):</b> {server_time}</p>
+    <pre>{top_output}</pre>
+    """
+
+if _name_ == '_main_':
     app.run(host='0.0.0.0', port=5000)
